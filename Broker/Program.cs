1c18ec61c5978d1a4be43fc10361357ea0f58dcf
Georgeta <ciorbageorgeta@gmail.com>
2021-09-19 15:13:42 +0300

    Add project files.

---
+++ Broker/Program.cs
@@ -0,0 +1,24 @@
+using Common;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Broker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls(EndpointsConstants.BrokerAddress)
+                .Build()
+                .Run();
+        }
+    }
+}
